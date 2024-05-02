4f94c28cf7742313afaf5cd020e441afe8456ed3
pySoo <soopy368@gmail.com>
2024-05-02 23:57:39 +0900

    [✨feat]: Input과 Textarea의 합성 컴포넌트인 TextField 공통 컴포넌트 추가

---
+++ src/components/Shared/TextField/index.tsx
@@ -0,0 +1,70 @@
+import {
+  Children,
+  cloneElement,
+  ForwardedRef,
+  forwardRef,
+  HTMLAttributes,
+  InputHTMLAttributes,
+  ReactElement,
+  ReactNode,
+} from 'react';
+
+import useId from '@/hooks/useId';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  label?: ReactNode;
+  children: ReactElement;
+}
+
+export function TextField({ label, children, ...props }: Props) {
+  const child = Children.only(children);
+  const id = child.props.id ?? useId();
+
+  return (
+    <div {...props}>
+      <label htmlFor={id}>{label}</label>
+      {cloneElement(child, {
+        id,
+        ...child.props,
+      })}
+    </div>
+  );
+}
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+TextField.Input = forwardRef(
+  (
+    { className, ...props }: InputProps,
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    return (
+      <input
+        ref={ref}
+        className={`w-full p-2 rounded-md border border-gray-300 rounded-md p-2 outline-none focus:border-[#FE6F0F] ${className}`}
+        {...props}
+      />
+    );
+  },
+);
+
+interface TextareaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
+  className?: string;
+}
+
+TextField.Textarea = forwardRef(
+  (
+    { className, ...props }: TextareaProps,
+    ref: ForwardedRef<HTMLTextAreaElement>,
+  ) => {
+    return (
+      <textarea
+        ref={ref}
+        className={`w-full p-2 rounded-md border border-gray-300 rounded-md p-2 outline-none focus:border-[#FE6F0F] resize-none ${className}`}
+        {...props}
+      />
+    );
+  },
+);
