44bc615761e85d42043d1f428980fdb3e109f361
pySoo <soopy368@gmail.com>
2024-06-14 23:47:51 +0900

    [✨feat]: next/server shopping API fetch 함수 추가

---
+++ src/app/api/getShoppingList/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import {
+  ERROR_MESSAGE,
+  SHOPPING_API_CLIENT_ID,
+  SHOPPING_API_CLIENT_SECRET,
+} from '../constants';
+
+const MOCK_QUERY = 'summer';
+
+export async function GET(request: NextRequest) {
+  const page = request.nextUrl.searchParams.get('page') ?? '1';
+
+  if (SHOPPING_API_CLIENT_ID == null || SHOPPING_API_CLIENT_SECRET == null)
+    return;
+
+  try {
+    const response = await fetch(
+      `https://openapi.naver.com/v1/search/shop.json?query=${MOCK_QUERY}&start=${page}`,
+      {
+        headers: {
+          'X-Naver-Client-Id': SHOPPING_API_CLIENT_ID,
+          'X-Naver-Client-Secret': SHOPPING_API_CLIENT_SECRET,
+        },
+      },
+    );
+
+    if (!response.ok) {
+      return NextResponse.json(null, { status: response.status });
+    }
+
+    return NextResponse.json(await response.json());
+  } catch (error) {
+    return NextResponse.json({ message: ERROR_MESSAGE[500] }, { status: 500 });
+  }
+}
