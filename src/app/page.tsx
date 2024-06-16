7cff333fbe0202c07d104955c197400d23ec1ee0
pySoo <soopy368@gmail.com>
2024-06-16 23:23:34 +0900

    [✨feat]: 메인 페이지 하단에 Navigation 컴포넌트 주입

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Navigation from '@/components/Navigation';
 import AsyncBoundary from '@/components/Shared/AsyncBoundary';
 import PageLayout from '@/components/Shared/Layout/PageLayout';
 import HydratedShoppingList from '@/components/Shopping/HydratedShoppingList';
 
 export default function Main() {
   return (
-    <PageLayout>
+    <PageLayout
+      className='pb-[70px]'
+      bottom={<Navigation />}
+    >
       <main>
         <AsyncBoundary>
           <HydratedShoppingList />
