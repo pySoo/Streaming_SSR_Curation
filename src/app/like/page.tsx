4f793025032994430e63bdbc10dc90d4a024acb6
pySoo <soopy368@gmail.com>
2024-06-18 23:32:23 +0900

    [✨feat]: 모든 page 컴포넌트를 서버 컴포넌트로 변경

--- src/app/like/page.tsx
+++ src/app/like/page.tsx
@@ -1,55 +1,13 @@
-'use client';
-
-import { useState } from 'react';
-
 import Navigation from '@/components/Navigation';
-import Button from '@/components/Shared/Button';
 import PageLayout from '@/components/Shared/Layout/PageLayout';
-import Text from '@/components/Shared/Text';
-import useLikeStore from '@/store/likeStore';
 
 import LikeListSection from './LikeListSection';
 
 export default function LikePage() {
-  const [isEditMode, setIsEditMode] = useState(false);
-
-  const setSelectedIds = useLikeStore((state) => state.setSelectedIds);
-  const deleteSelectedItem = useLikeStore((state) => state.deleteSelectedItem);
-
-  const toggleEditMode = () => {
-    setIsEditMode(!isEditMode);
-    setSelectedIds([]);
-  };
-
-  const handleDeleteItem = () => {
-    deleteSelectedItem();
-    setIsEditMode(false);
-  };
-
   return (
-    <PageLayout
-      className='pb-[70px]'
-      bottom={
-        <Navigation>
-          {isEditMode ? (
-            <Text
-              onClick={handleDeleteItem}
-              className='flex items-center font-semibold cursor-pointer'
-            >
-              상품 삭제
-            </Text>
-          ) : null}
-        </Navigation>
-      }
-    >
-      <Button
-        variant='secondary'
-        className='w-fit px-1 font-semibold ml-auto'
-        onClick={toggleEditMode}
-      >
-        {isEditMode ? '취소' : '편집'}
-      </Button>
-      <LikeListSection isEditMode={isEditMode} />
+    <PageLayout>
+      <LikeListSection />
+      <Navigation />
     </PageLayout>
   );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,46 +1,13 @@
-'use client';
-
-import { useState } from 'react';
-
 import Navigation from '@/components/Navigation';
-import Button from '@/components/Shared/Button';
 import PageLayout from '@/components/Shared/Layout/PageLayout';
-import Stack from '@/components/Shared/Layout/Stack';
-import SearchBar from '@/components/Shared/SearchBar';
-import Text from '@/components/Shared/Text';
-import ShoppingListSection from '@/components/Shopping/ShoppingListSection';
 
-interface Props {
-  searchParams?: {
-    q?: string;
-  };
-}
+import SearchSection from './SearchSection';
 
-export default function SearchPage({ searchParams }: Props) {
-  const [query, setQuery] = useState(searchParams?.q ?? '');
-
-  const handleSubmit = (query: string) => {
-    setQuery(query);
-  };
-
+export default function SearchPage() {
   return (
-    <PageLayout
-      className='pb-[70px]'
-      bottom={<Navigation />}
-    >
-      <Stack className='h-full'>
-        <Stack.Row className='w-full gap-2'>
-          <SearchBar onSubmit={handleSubmit} />
-          <Button>검색</Button>
-        </Stack.Row>
-        <Stack className='flex justify-center items-center mt-2'>
-          {query ? (
-            <ShoppingListSection query={query} />
-          ) : (
-            <Text variant='caption'>원하는 상품을 검색해 보세요!</Text>
-          )}
-        </Stack>
-      </Stack>
+    <PageLayout className='pb-[70px]'>
+      <SearchSection />
+      <Navigation />
     </PageLayout>
   );
 }
