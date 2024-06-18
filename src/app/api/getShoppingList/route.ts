a1be8229c6f1a2a180dac06d3cf6aff5c84a9ef1
pySoo <soopy368@gmail.com>
2024-06-18 18:46:09 +0900

    [✨feat]: query 값이 존재하지 않을 경우 예외처리 추가

--- src/app/api/getShoppingList/route.ts
+++ src/app/api/getShoppingList/route.ts
@@ -10,13 +10,16 @@ const MOCK_QUERY = 'summer';
 
 export async function GET(request: NextRequest) {
   const page = request.nextUrl.searchParams.get('page') ?? '1';
+  const query = request.nextUrl.searchParams.get('query');
+  const searchQuery =
+    query === null || query === 'undefined' ? MOCK_QUERY : query;
 
   if (SHOPPING_API_CLIENT_ID == null || SHOPPING_API_CLIENT_SECRET == null)
     return;
 
   try {
     const response = await fetch(
-      `https://openapi.naver.com/v1/search/shop.json?query=${MOCK_QUERY}&start=${page}`,
+      `https://openapi.naver.com/v1/search/shop.json?query=${searchQuery}&start=${page}`,
       {
         headers: {
           'X-Naver-Client-Id': SHOPPING_API_CLIENT_ID,
