c6ea1a454ac4a5f26a4aceb405bc6f50ad5f1f04
pySoo <soopy368@gmail.com>
2024-06-21 22:54:03 +0900

    [✨feat]: API key, 날씨 상태별 한글 title, icon 이미지 맵핑 상수 추가

--- src/app/api/constants.ts
+++ src/app/api/constants.ts
@@ -1,3 +1,5 @@
+import { WeatherDescription } from './types';
+
 export const ERROR_MESSAGE = {
   400: '잘못된 요청입니다',
   401: '로그인이 필요합니다',
@@ -10,3 +12,40 @@ export const SHOPPING_API_CLIENT_ID =
 
 export const SHOPPING_API_CLIENT_SECRET =
   process.env.NEXT_PUBLIC_SHOPPING_API_CLIENT_SECRET;
+
+export const WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+
+export const WEATHER_DESCRIPTION: Record<string, WeatherDescription> = {
+  clear: {
+    title: '맑음',
+    icon: '01d',
+  },
+  'partly cloudy': {
+    title: '구름 조금',
+    icon: '02d',
+  },
+  cloudy: {
+    title: '흐림',
+    icon: '03d',
+  },
+  rain: {
+    title: '비',
+    icon: '10d',
+  },
+  snow: {
+    title: '눈',
+    icon: '13d',
+  },
+  sleet: {
+    title: '진눈깨비',
+    icon: '13d',
+  },
+  wind: {
+    title: '바람',
+    icon: '50d',
+  },
+  fog: {
+    title: '안개',
+    icon: '50d',
+  },
+};
