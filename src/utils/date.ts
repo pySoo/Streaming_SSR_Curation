43655098c6c7a3c19bf1205e47fc2d31512eb226
pySoo <soopy368@gmail.com>
2024-06-24 23:27:35 +0900

    [✨feat]: timestamp를 통해 오늘인지 파악하는 util 함수 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -16,3 +16,14 @@ export const convertTimestampToDate = (
 
   return convertDateByFormat(currentDate, dateFormat ?? 'MM/DD (ddd)');
 };
+
+export const isToday = (timestamp: number) => {
+  const today = new Date();
+  const currentDate = new Date(timestamp * 1000);
+
+  return (
+    today.getFullYear() === currentDate.getFullYear() &&
+    today.getMonth() === currentDate.getMonth() &&
+    today.getDate() === currentDate.getDate()
+  );
+};
