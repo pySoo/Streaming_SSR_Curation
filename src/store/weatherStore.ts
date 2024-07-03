d9ce159ca107eb05953bedab0a80a38723ab6c11
pySoo <soopy368@gmail.com>
2024-07-03 21:08:46 +0900

    [✨feat]: 현재 날씨와 오늘의 날씨를 분리하기 위해 todayWeather state 추가

--- src/store/weatherStore.ts
+++ src/store/weatherStore.ts
@@ -5,17 +5,21 @@ import { Weather } from '@/app/api/types';
 
 interface Props {
   currentWeather: Weather | null;
+  todayWeather: Weather | null;
   weatherList: Weather[];
   setCurrentWeather: (currentWeather: Weather | null) => void;
+  setTodayWeather: (todayWeather: Weather | null) => void;
   setWeatherList: (weatherList: Weather[]) => void;
 }
 
 const useWeatherStore = create<Props>()(
   devtools((set) => ({
     currentWeather: null,
+    todayWeather: null,
     weatherList: [],
     setCurrentWeather: (currentWeather: Weather | null) =>
       set({ currentWeather }),
+    setTodayWeather: (todayWeather: Weather | null) => set({ todayWeather }),
     setWeatherList: (weatherList: Weather[]) => set({ weatherList }),
   })),
 );
