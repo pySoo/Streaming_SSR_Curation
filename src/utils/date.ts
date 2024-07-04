eb304cfb0ad02f0113c13459048be7ba586fb5c5
pySoo <soopy368@gmail.com>
2024-07-05 01:50:39 +0900

    [♻️refactor]: Date 객체를 dayjs 함수 사용으로 변경

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,27 +4,27 @@ import dayjs from 'dayjs';
 
 dayjs.locale('ko');
 
-export const convertDateByFormat = (date: Date, dateFormat?: string) => {
-  return dayjs(date).format(dateFormat ?? 'YYYY.MM.DD');
+export const convertDateByFormat = (date: dayjs.Dayjs, dateFormat: string) => {
+  return date.format(dateFormat);
 };
 
 export const convertTimestampToDate = (
   timestamp: number,
   dateFormat?: string,
 ) => {
-  const currentDate = new Date(timestamp * 1000);
+  const currentDate = dayjs(timestamp * 1000);
 
   return convertDateByFormat(currentDate, dateFormat ?? 'MM/DD (ddd)');
 };
 
 export const isToday = (timestamp: number) => {
-  const today = new Date();
-  const currentDate = new Date(timestamp * 1000);
+  const today = dayjs();
+  const currentDate = dayjs(timestamp * 1000);
 
   return (
-    today.getFullYear() === currentDate.getFullYear() &&
-    today.getMonth() === currentDate.getMonth() &&
-    today.getDate() === currentDate.getDate()
+    today.year() === currentDate.year() &&
+    today.month() === currentDate.month() &&
+    today.date() === currentDate.date()
   );
 };
 
