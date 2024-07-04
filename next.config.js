e490256b86c71bef085015738ae635e79e67605f
pySoo <soopy368@gmail.com>
2024-07-05 03:55:29 +0900

    [✨feat]: avif 포맷을 우선적으로 사용하도록 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    formats: ['image/avif', 'image/webp'],
     imageSizes: [16, 32],
     deviceSizes: [250, 500],
     remotePatterns: [
