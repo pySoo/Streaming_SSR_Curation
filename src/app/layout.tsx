5f769fa51ec6f723fb16d1b6a8c0214a822c6126
pySoo <soopy368@gmail.com>
2024-07-04 15:39:36 +0900

    [✨feat]: ToastProvider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import ReactQueryProvider from '@/components/ReactQueryProvider';
+import ToastProvider from '@/components/Shared/Toast/ToastProvider';
 
 export default function RootLayout({
   children,
@@ -10,7 +11,10 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <body suppressHydrationWarning={true}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <ReactQueryProvider>
+          {children}
+          <ToastProvider />
+        </ReactQueryProvider>
       </body>
     </html>
   );
