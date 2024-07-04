20b3a820be6e5b36e79c7547054a36d0b3182b3a
pySoo <soopy368@gmail.com>
2024-07-04 16:08:18 +0900

    [♻️refactor]: searchParams 코드 제거, Button을 SearchBar 내부로 이동

--- src/app/search/SearchSection.tsx
+++ src/app/search/SearchSection.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
-import Button from '@/components/Shared/Button';
+import AsyncBoundary from '@/components/Shared/AsyncBoundary';
 import Stack from '@/components/Shared/Layout/Stack';
 import SearchBar from '@/components/Shared/SearchBar';
+import Spacing from '@/components/Shared/Spacing';
 import Text from '@/components/Shared/Text';
+import ProductListSkeleton from '@/components/Shopping/ProductListSkeleton';
 import ShoppingListSection from '@/components/Shopping/ShoppingListSection';
 
 export default function SearchSection() {
-  const searchParams = useSearchParams();
-  const [query, setQuery] = useState(searchParams.get('q') ?? '');
+  const [query, setQuery] = useState('');
 
   const handleSubmit = (query: string) => {
     setQuery(query);
@@ -20,18 +20,30 @@ export default function SearchSection() {
   return (
     <section>
       <Stack className='h-full'>
-        <Stack.Row className='w-full gap-2'>
-          <SearchBar
-            inputProps={{ value: query }}
-            onSubmit={handleSubmit}
-          />
-          <Button>검색</Button>
-        </Stack.Row>
-        <Stack className='flex justify-center items-center mt-2'>
-          {query ? (
-            <ShoppingListSection query={query} />
-          ) : (
-            <Text variant='caption'>원하는 상품을 검색해 보세요!</Text>
+        <Spacing size={20} />
+        <Stack>
+          <SearchBar onSubmit={handleSubmit} />
+          <Spacing size={10} />
+          {query && (
+            <Text
+              variant='subtitle'
+              className='text-left'
+            >
+              {query} 검색 결과
+            </Text>
+          )}
+        </Stack>
+        <Spacing size={10} />
+        <Stack className='w-full'>
+          {query && (
+            <AsyncBoundary
+              suspenseFallback={<ProductListSkeleton variant='shopping' />}
+            >
+              <ShoppingListSection query={query} />
+            </AsyncBoundary>
+          )}
+          {!query && (
+            <Text className='text-center'>원하는 상품을 검색해 보세요 🔍</Text>
           )}
         </Stack>
       </Stack>
