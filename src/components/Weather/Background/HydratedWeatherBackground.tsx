56fa561ee7e5d1bf984f37bf58d2fc72a24d0c88
pySoo <soopy368@gmail.com>
2024-07-05 03:52:04 +0900

    [✨feat]: 캐싱된 데이터를 사용할 수 있게 하는 Hydrated 컴포넌트 추가

---
+++ src/components/Weather/Background/HydratedWeatherBackground.tsx
@@ -0,0 +1,22 @@
+import { dehydrate, Hydrate } from '@tanstack/react-query';
+
+import getQueryClient from '@/app/api/queries/getQueryClient';
+import { QUERY_KEYS } from '@/app/api/queries/queryKey';
+import { getCurrentWeather } from '@/hooks/apis/getWeatherList';
+
+import WeatherBackground from './WeatherBackground';
+
+export default async function HydratedWeatherBackground() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery([QUERY_KEYS.WEATHER.CURRENT], () =>
+    getCurrentWeather(),
+  );
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <Hydrate state={dehydratedState}>
+      <WeatherBackground />
+    </Hydrate>
+  );
+}
