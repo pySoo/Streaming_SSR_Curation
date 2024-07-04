816bb51a88ffe5ec2b960c455f2fcb55248a4aa1
pySoo <soopy368@gmail.com>
2024-07-04 17:12:53 +0900

    [✨feat]: metadata 내용 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,19 @@ import './globals.css';
 import ReactQueryProvider from '@/components/ReactQueryProvider';
 import ToastProvider from '@/components/Shared/Toast/ToastProvider';
 
+export const metadata = {
+  title: 'Weather Curation ☀️',
+  description:
+    '기온에 맞는 옷과 주간 날씨 특징에 따른 상품을 추천하는 큐레이션 서비스입니다.',
+  metadataBase: new URL('https://weather-curation-streamiing.vercel.app'),
+  openGraph: {
+    title: 'Weather Curation ☀️',
+    description:
+      '기온에 맞는 옷과 주간 날씨 특징에 따른 상품을 추천하는 큐레이션 서비스입니다.',
+    url: 'https://weather-curation-streamiing.vercel.app',
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
