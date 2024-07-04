722d1a48be71ddc78beede8fdee4ffbe41b0ab51
pySoo <soopy368@gmail.com>
2024-07-04 14:35:34 +0900

    [♻️refactor]: 북마크 요소가 있는 UI를 LikeEditSection으로 분리

--- src/app/like/LikeListSection.tsx
+++ src/app/like/LikeListSection.tsx
@@ -1,88 +1,18 @@
 'use client';
 
-import { useState } from 'react';
+import Link from 'next/link';
 
 import Button from '@/components/Shared/Button';
-import List from '@/components/Shared/Layout/List';
 import Stack from '@/components/Shared/Layout/Stack';
+import Spacing from '@/components/Shared/Spacing';
 import Text from '@/components/Shared/Text';
-import ProductCard from '@/components/Shopping/ProductCard';
-import ProductCheckBox from '@/components/Shopping/ProductCheckBox';
+import { PATH } from '@/constants/path';
 import useLikeStore from '@/store/likeStore';
 
-import { ProductId } from '../api/types';
-
-interface Props {
-  isEditMode: boolean;
-}
+import LikeEditSection from './LikeEditSection';
 
 export default function LikeListSection() {
-  const [isEditMode, setIsEditMode] = useState(false);
-
   const likeList = useLikeStore((state) => state.likeList);
 
-  const selectedIds = useLikeStore((state) => state.selectedIds);
-  const setSelectedIds = useLikeStore((state) => state.setSelectedIds);
-  const deleteSelectedItem = useLikeStore((state) => state.deleteSelectedItem);
-
-  const toggleEditMode = () => {
-    setIsEditMode(!isEditMode);
-    setSelectedIds([]);
-  };
-
-  const handleSelectCheckBox = (id: ProductId, isSelected: boolean) => {
-    const filteredIds = isSelected
-      ? [...selectedIds, id]
-      : selectedIds.filter((selectedId) => selectedId !== id);
-
-    setSelectedIds(filteredIds);
-  };
-
-  const handleDeleteItem = () => {
-    deleteSelectedItem();
-    setIsEditMode(false);
-  };
-
-  return (
-    <section>
-      <Stack>
-        <Button
-          variant='secondary'
-          className='w-fit px-1 font-semibold ml-auto'
-          onClick={toggleEditMode}
-        >
-          {isEditMode ? '취소' : '편집'}
-        </Button>
-        <List className='grid grid-cols-2'>
-          {likeList.map((product, index) => (
-            <List.Row
-              key={product.productId + index}
-              className='items-start'
-            >
-              <ProductCard
-                product={product}
-                right={
-                  isEditMode ? (
-                    <ProductCheckBox
-                      id={product.productId}
-                      value={selectedIds.includes(product.productId)}
-                      onSelect={handleSelectCheckBox}
-                    />
-                  ) : null
-                }
-              />
-            </List.Row>
-          ))}
-        </List>
-      </Stack>
-      {isEditMode ? (
-        <Text
-          onClick={handleDeleteItem}
-          className='flex items-center font-semibold cursor-pointer'
-        >
-          상품 삭제
-        </Text>
-      ) : null}
-    </section>
-  );
+  return <section>{likeList.length > 0 && <LikeEditSection />}</section>;
 }
