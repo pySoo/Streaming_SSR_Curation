77593c81ad34a46721c139700405148225b393cb
pySoo <soopy368@gmail.com>
2024-07-11 20:05:07 +0900

    [✨feat]: useEffect 제거, useBackground hook을 사용하도록 변경

--- src/components/Weather/Background/WeatherBackground.tsx
+++ src/components/Weather/Background/WeatherBackground.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
 import { useGetCurrentWeather } from '@/hooks/apis/getWeatherList';
-import { getDayTime } from '@/utils/date';
+import { useBackground } from '@/hooks/useBackground';
 
 import { Cloud, Rain, Snow, Stars, Sun } from './Animation';
-import { getCurrentSkyColor, getDefaultSkyColor } from './utils';
 
 export default function WeatherBackground() {
-  const [isDay, setIsDay] = useState(true);
-  const [skyColor, setSkyColor] = useState(getDefaultSkyColor());
   const { data: currentWeather } = useGetCurrentWeather();
+  const { dayTime, currentSkyColor } = useBackground();
+
+  const isNight = dayTime === 'night';
 
   const weatherSummary = currentWeather?.summary;
 
@@ -22,20 +20,15 @@ export default function WeatherBackground() {
         ? 30
         : 0;
 
-  useEffect(() => {
-    setIsDay(getDayTime() === 'day');
-    setSkyColor(getCurrentSkyColor());
-  }, []);
-
   return (
     <div
       className='w-full text-lg h-full min-h-screen absolute top-0 left-0 pb-navigation'
       style={{
-        background: `linear-gradient(${skyColor})`,
+        background: `linear-gradient(${currentSkyColor})`,
       }}
     >
-      {isDay && weatherSummary === 'clear' && <Sun UVindex={60} />}
-      {!isDay && <Stars />}
+      {!isNight && weatherSummary === 'clear' && <Sun UVindex={60} />}
+      {isNight && <Stars />}
       {cloudCover > 0 && <Cloud cloudCover={cloudCover} />}
       {weatherSummary === 'rain' && <Rain />}
       {weatherSummary === 'snow' && <Snow />}
