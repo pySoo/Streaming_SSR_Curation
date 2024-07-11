3e8c479290274f9bdef2362678eac3be5927d771
pySoo <soopy368@gmail.com>
2024-07-11 17:16:15 +0900

    [✨feat]: 현재 날씨는 정각마다 업데이트, 날씨 리스트는 하루에 한 번 업데이트 되도록 queryKey 설정

--- src/app/api/queries/queryKey.ts
+++ src/app/api/queries/queryKey.ts
@@ -1,9 +1,11 @@
+import { getCurrentDate, getCurrentDateAndHour } from '@/utils/date';
+
 export const QUERY_KEYS = {
   SHOPPING: {
     LIST: (query: string) => ['SHOPPING_LIST', query],
   },
   WEATHER: {
-    CURRENT: ['WEATHER_CURRENT'],
-    LIST: ['WEATHER_LIST'],
+    CURRENT: ['WEATHER_CURRENT', getCurrentDateAndHour()],
+    LIST: ['WEATHER_LIST', getCurrentDate()],
   },
 } as const;
